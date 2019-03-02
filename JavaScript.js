d3e9d20e5111d58aa29a008e58c284d3713f2bee
Sikki <44468195+sikandarportfolio@users.noreply.github.com>
2019-03-02 23:04:19 +0500

    Working cards

--- JavaScript.js
+++ JavaScript.js
@@ -16,10 +16,9 @@ url2=nameField;
 
 // after this generates URL with user input
 var script = document.createElement("script");
-//alert('bookAsk');
 script.src = url + url2 + url3
 document.body.appendChild(script);
-// 
+
 }
 
 var subButton = document.getElementById('subButton');   //Adss book name in dispaly
@@ -29,9 +28,18 @@ subButton.addEventListener('click', getUserName, false);
 
 
 function handleResponse(response) {                   //populates search results
-for (var i = 0; i < response.items.length; i++) {
+for (var i = 0; i < response.items.length -5; i++) {
   var item = response.items[i];
   // in production code, item.text should have the HTML entities escaped.
+
+ // document.getElementById("pic").src=item.volumeInfo.imageLinks.thumbnail;
   document.getElementById("content").innerHTML += "<br>" + item.volumeInfo.title;
+  document.getElementById("content").innerHTML += "<br>" +"Written by: "+ item.volumeInfo.authors;
+  document.getElementById("content").innerHTML += "<br>" +"Published by: "+ item.volumeInfo.publisher+                                                                         
+                                                "<p>--------------------------------------------------</p>";
+ 
+
+
 }
 }
+//Each item in the list should include the book's author, title, and publishing company, as well as a picture of the book.
