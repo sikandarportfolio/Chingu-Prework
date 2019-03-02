2a90fae1d4c0d57762052e374ecb89b5f7e649ae
Sikki <44468195+sikandarportfolio@users.noreply.github.com>
2019-03-02 16:07:26 +0500

    Url is live

--- JavaScript.js
+++ JavaScript.js
@@ -1,2 +1,39 @@
-//alert("Welcome to the CSS lesson series")
+
+//Make the url here 
+var url="https://www.googleapis.com/books/v1/volumes?q=jon+snow&callback=handleResponse";
+var url2="";
+var url3="";
+var script = document.createElement("script");
+script.src = url
+document.body.appendChild(script);
+//Make the url here
+
+var nameField;
+function getUserName() {
+nameField = document.getElementById('nameField').value;
+var result = document.getElementById('result');
+
+if (nameField.length < 3) {
+result.textContent = 'Book name must contain at least 3 characters';
+//alert('Username must contain at least 3 characters');
+} else {
+result.textContent = 'Your Book name is: ' + nameField;
+//alert(nameField);
+}
+}
+
+var subButton = document.getElementById('subButton');   //Adss book name in dispaly
+subButton.addEventListener('click', getUserName, false); 
+
+
+
+function handleResponse(response) {
+for (var i = 0; i < response.items.length; i++) {
+  var item = response.items[i];
+  // in production code, item.text should have the HTML entities escaped.
+  document.getElementById("content").innerHTML += "<br>" + item.volumeInfo.title;
+}
+}
+
+
 
